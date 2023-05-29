15c50a581a30a9dca96b2e2ab211adb45042a88d
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-28 23:45:41 -0400

    most recent changes to file

--- express_server.js
+++ express_server.js
@@ -27,5 +27,21 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
+  app.get('/', function(req, res) {
+    var mascots = [
+      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+      { name: 'Tux', organization: "Linux", birth_year: 1996},
+      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+  
+    res.render('pages/index', {
+      mascots: mascots,
+      tagline: tagline
+    });
+  });
   res.render("urls_index", templateVars);
 });
+<%- include('partials/_head') %>
+<%- include('partials/_header') %>
+<%- include('partials/_footer') %>
